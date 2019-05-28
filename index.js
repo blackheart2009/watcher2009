598487dc88447dde1c8211658bc18f42d0f48b44
Sanjeeb Kumar Sahoo <50053860+blackheart2009@users.noreply.github.com>
2019-05-28 13:32:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const vm = require('vm');
 const codeContext = {};
 vm.createContext(codeContext);
 
-const token = '';
+const token = 'NTcwOTEzMTE2ODkyOTU0NjI0.XOzBHw.L8PPc8N59Ee1V5-ATf0BKSdDegM';
 
 const prefix = '!';
 
@@ -44,14 +44,47 @@ bot.on('message', message => {
     let args = message.content.toLowerCase().substring(prefix.length).split(" ");
 
     switch (args[0]) {
-        case 'cclink':
-            message.channel.send('http://kuilin.net/cc_n/clan.php?tag=L2RJC0CV');
-            break;
-        case  'fwastatslink':
-            message.channel.send('http://fwastats.com/Clan/L2RJC0CV');
+        case 'implinks':
+            message.channel.send(`🔥Hey there, Here is few important link used in farm wars.🔥
+
+            💎IMPORTANT LINKS💎:-
+            
+            ☘Clan chocolate clash link-
+            📌ÇHÀMPÎÕÑS
+            http://kuilin.net/cc_n/clan.php?tag=L2RJC0CV
+                   
+            ☘Clan Link through Fwastats:-
+            
+            📌ÇHÀMPÎÕÑS https://fwastats.com/Clan/L2RJC0CV
+        
+            ☘Chocolate clash link for searching players in member lookup:-
+            http://kuilin.net/cc_n
+            
+            ☘Clan donation tracker link
+            (check from here who donated wrong) :-
+            
+            📌 ÇHÀMPÎÕÑS
+             ✳ http://tiny.cc/Championstracker
+            
+            ☘Clan rules guide link:- 
+            
+            ✳https://band.us/band/62858363/post/61
+            
+            
+            ✳https://band.us/band/62858363/post/89
+            
+            ☘FWA GUIDE:-
+            
+            📌sites.google.com/site/fwaguide
+            
+            🔥Hindi FWA GUIDE:- 
+            
+            📌1Hindi: https://drive.google.com/open?id=1UNlHjWL3kMSx4g_6FZ3x9IoJVMi_LHMpc9ciSJHlsTg
+            
+            📌2Hindi: https://drive.google.com/open?id=1fXjbFXRdtdWT-5YwEE0cyr0QFGoCghtix7rQ4YnxRwg`);
             break;
         case 'clanlink':
-            message.channel.send('https://link.clashofclans.com/?action=OpenClanProfile&tag=L2RJC0CV');
+            message.channel.search('https://link.clashofclans.com/?action=OpenClanProfile&tag=L2RJC0CV');
             break; 
         case  'recruitmentpost':
             message.channel.send(`💎OFFICIAL FWA💎  
@@ -93,7 +126,7 @@ bot.on('message', message => {
             🎶🎶🎶Pushing Again to go back to top India 🎶🎶🎶
             
             📢 Primary Language:-Hindi/English.`);
-            break;                
+            break;        
         case 'botinfo':
             message.channel.send('Watcher is designed for CHAMPIONS clan. Messages sent by the user will be displayed there and sending a message in that channel will also forward it to that user. This bot is different from the others as it is the one and only private bot, design for CHAMPIONS clan only. This means that it is ensured to be hosted 24/7 without you having to pay extra hosting fees.');
             break;
@@ -103,6 +136,54 @@ bot.on('message', message => {
             if (!args[1]) return message.reply('Error plese define second arg');
             message.channel.bulkDelete(args[1]);
             break;
+        case  'clanrules':
+            message.channel.send(`📜CLAN RULES: 
+
+            General:
+                •	🐝Be active. Be mature. Be respectful of others.
+                •	🐝Be polite - saying please and thank you will make your mother (and us) proud.
+                •	🎟Opting out is not a permanent state - only temporary.
+                •	🛡Get in a league as soon as possible when season starts -don't go around naked.
+                •	📮If you won't be able to war, leave us a note on BAND and go red (opt out).
+                •	😡Failure to comply with rules may result in warning, penalties or removal.
+                •	👤New members must join our BAND site and have a Co-Leader approve your FWA-compliant base when joining the clan. 
+            
+            Donations and Requests:
+                •	🔁Donate and request at least 1500 per season.
+                •	🔂Donate at least 1 for every 3 you request. 
+                •	⏸Respect request and only donate what is requested.
+                •	⏹If you mess up your donation, admit it and apologize.
+                •	↕Donate the largest troop first to ensure it will fit.
+                •	⏺Don't ask only for troops you cannot make or don't donate yourself.
+                •	⏩Fill open requests before you request yourself.
+            
+            War:
+                •	📩Watch clan mail for instructions to 2 or 3 star if we match FWA or if we mismatch (very rare).
+                •	⏰Attack in the first 12 hours of war.
+                •	⚔Make both attacks in every war.
+                •	❗Always attack your mirror on your first attack with 2 or 3 stars depending on instructions. (💂🏻‍♀Duty before booty💰.) (unless you using STW program as a Thall 9)
+                •	‼For your second attack, hit the highest base that has already been hit and starred by its mirror or wait until the last 12 hours of war and hit any base.
+                •	🕯Even if someone has hit your base because you are late, you are still expected to attack your mirror in the same way as if it had not already been hit.
+                •	✨You may clean up someone else's mirror at any time after they have screwed it up.
+                •	🤦🏼‍♂If you innocently loot 1 & 2 because you don't know any better (i.e., been living on a foreign planet until lately), You will need an good excuse or be kicked.
+                •	💋If we mismatch, leave your FWA base up unless told otherwise (extremely rare).
+                •	🤦🏻‍♀Never put troops in the war castles in a FWA war.
+            
+            Clan Games:
+                 •  1⃣0⃣0⃣0⃣1000 minimum clan game points each game
+                 •  🚫Do not participate if you won’t get the minimum.  It’s not fair to rest of team if you do one game and get all the rewards. We are a team
+            
+            Earning Elder:
+                •	🏪Be active in chat.
+                •	🏦Be a high donor.
+                •	💒Answer questions and help out your mates.
+            
+            Earning Co-Leader:
+                •	👨🏻‍💻Be willing to roll your sleeves up and work like a fiend for the betterment of the clan and FWA.
+                •	👩🏼‍💻Be willing to do any one or more of the administrative tasks need to ensure our ongoing membership with FWA.
+            
+            👉🏻Please acknowledge below that you have read and understand the above.👇🏻`);
+            break;            
     }
     switch (args[0]) {
         case 'clanprofile':
@@ -112,10 +193,6 @@ bot.on('message', message => {
         case 'clanlogo':
             const attachment2 = new Attachment('./My-clan-logo.png');
             message.channel.send(message.author, attachment2);
-            break;
-        case 'rules':
-            const attachment3 = new Attachment('./rules.txt');
-            message.channel.send(message.author, attachment3);
             break;
     }
     switch (args[0]) {
